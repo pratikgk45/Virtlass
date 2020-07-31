753c8dd27642795ca2950fb7628f62c67ecd3715
Pratik <kalepg45@gmail.com>
2020-07-31 15:13:34 +0530

    preloader updated

--- static/js/canvas.js
+++ static/js/canvas.js
@@ -209,20 +209,7 @@ function rotate(){
 	document.body.appendChild(css);
 }
 
-const preloader = document.querySelector('.preloader');
-const fadeEffect = setInterval(() => {
-	if(!preloader.style.opacity){
-		preloader.style.opacity = 1;
-	}
-	if(preloader.style.opacity > 0) {
-		preloader.style.opacity -= 0.1;
-	}else{
-		clearInterval(fadeEffect);
-	}
-}, 20);
-
 window.onload = function() {
-	$(".preloader").delay(2000).fadeOut(1000);
 	// init();
 	rotate();
 	$(".scramble_text").scramble(5000, 20, "all", true);
